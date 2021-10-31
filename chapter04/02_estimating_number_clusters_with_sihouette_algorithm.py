22337c94cb202aa1c2cc80c21e84263f57d2fa8f
ww166 <16602115911@163.com>
2021-10-31 09:54:00 +0800

    1. Estimating number clusters of the data with Silhouette Score Algorithm.

---
+++ chapter04/02_estimating_number_clusters_with_sihouette_algorithm.py
@@ -0,0 +1,53 @@
+import numpy as np
+import matplotlib.pyplot as plt
+from sklearn import metrics
+from sklearn.cluster import KMeans
+from sklearn import metrics
+
+# Load input data
+X = np.loadtxt('data/data_quality.txt', delimiter=',')
+
+# Initialize variables
+scores = []
+values = np.arange(2, 10)
+
+# Iterate through the defined range
+for num_clusters in values:
+    # Train the KMeans clusters model
+    kmeans = KMeans(init='k-means++', n_clusters=num_clusters, n_init=10)
+    kmeans.fit(X)
+
+    score = metrics.silhouette_score(X, kmeans.labels_,
+                                     metric='euclidean',
+                                     sample_size=len(X))
+    print('\nNumber of clusters = ', num_clusters)
+    print('Silhouette score = ', score)
+    scores.append(score)
+
+
+# Plot silhouette scores
+plt.figure()
+plt.bar(values, scores, width=0.7, color='black', align='center')
+plt.title('Silhouette score vs number of clusters')
+
+# Extract best socre and optimal number of clusters
+num_clusters = np.argmax(scores) + values[0]
+print('\nOptimal number of clusters = ', num_clusters)
+
+# Plot data
+plt.figure()
+plt.scatter(X[:, 0], X[:, 1], color='black', s=80, marker='o', facecolor='none')
+
+x_min, x_max = X[:, 0].min() - 1, X[:, 0].max() + 1
+y_min, y_max = X[:, 1].min() - 1, X[:, 1].max() + 1
+
+plt.title('Input data')
+plt.xlim(x_min, x_max)
+plt.ylim(y_min, y_max)
+
+plt.xticks()
+plt.yticks()
+
+plt.show()
+
+
